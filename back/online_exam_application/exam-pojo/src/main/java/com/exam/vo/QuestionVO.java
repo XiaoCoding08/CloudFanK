7ef5c6ce639b8e6ee5d39bc64a7c02c51aafefda
felix <1534589097@qq.com>
2023-12-21 12:05:48 +0800

    教师题目管理
    教师题目管理

---
+++ back/online_exam_application/exam-pojo/src/main/java/com/exam/vo/QuestionVO.java
@@ -0,0 +1,60 @@
+package com.exam.vo;
+
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 题目表 VO
+ *
+ * @author felix
+ * @since 2023-12-21 20:52:11
+ */
+@Data
+@Builder
+public class QuestionVO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    /**
+     * 题号
+     */
+    @ApiModelProperty("题号")
+    private Long id;
+    /**
+     * 课程号
+     */
+    @ApiModelProperty("课程号")
+    private Long courseId;
+    /**
+     * 题型
+     */
+    @ApiModelProperty("题型")
+    private Integer type;
+    /**
+     * 内容
+     */
+    @ApiModelProperty("内容")
+    private String context;
+    /**
+     * 图片地址
+     */
+    @ApiModelProperty("图片地址")
+    private String imageUrl;
+    /**
+     * 答案
+     */
+    @ApiModelProperty("答案")
+    private String answer;
+    /**
+     * 创建时间
+     */
+    @ApiModelProperty("创建时间")
+    private Date createTime;
+}
+
--- back/online_exam_application/exam-server/src/main/java/com/exam/service/QuestionService.java
+++ back/online_exam_application/exam-server/src/main/java/com/exam/service/QuestionService.java
@@ -1,9 +1,26 @@
 package com.exam.service;
 
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.exam.dto.AddQuestionDTO;
+import com.exam.dto.QuestionPageQueryDTO;
+import com.exam.dto.UpdateQuestionDTO;
 import com.exam.entity.Question;
+import com.exam.vo.QuestionVO;
+
+import java.util.List;
 
 public interface QuestionService extends IService<Question> {
     Long add(AddQuestionDTO addQuestionDTO);
+
+    void del(Long id);
+
+    void updateOne(UpdateQuestionDTO updateQuestionDTO);
+
+    QueryWrapper<Question> getQueryWrapper(QuestionPageQueryDTO questionPageQueryDTO);
+
+    List<QuestionVO> getQueryVOList(List<Question> questionList);
+
+    public QuestionVO  getQuestionVO(Question question);
 }
